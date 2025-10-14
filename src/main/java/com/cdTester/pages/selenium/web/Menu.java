8d5f3db142ee94a49a4a73b1393de115130231b6
CDTester <chris_a_day@hotmail.com>
2025-10-14 20:03:33 +0100

    Added more tests and highlights

--- src/main/java/com/cdTester/pages/selenium/web/Menu.java
+++ src/main/java/com/cdTester/pages/selenium/web/Menu.java
@@ -1,5 +1,6 @@
 package com.cdTester.pages.selenium.web;
 
+import com.cdTester.utils.Highlight;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -39,6 +40,17 @@ public class Menu {
   public Menu(WebDriver driver) {
     this.driver = driver;
     PageFactory.initElements(driver, this);
+  }
+
+  /**
+   * Highlights a web element on the page except checkboxes and radio buttons.
+   * @param element The WebElement to be highlighted.
+   * @return The same WebElement after highlighting.
+   * @throws InterruptedException If the thread is interrupted during sleep.
+   */
+  public WebElement highlightElement(WebElement element) throws InterruptedException {
+    Highlight.highlightElement(this.driver, element);
+    return element;
   }
 
   public String getMenuType() {
